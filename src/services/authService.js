e05afccc10c6a80bc40828221d8e2781204fe497
Chandramouli M <130632952+Charanstark3@users.noreply.github.com>
2024-10-08 19:34:19 +0530

    Update authService.js

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/auth';
+const API_URL = 'https://guvi-project-backend.onrender.com';
 
 export const login = async (credentials) => {
     const response = await axios.post(`${API_URL}/login`, credentials);
